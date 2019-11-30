fa43d3241c5f81c9a5ec044b39fc1f732fc5d157
avishka meddegoda <ameddegoda143@gmail.com>
2019-12-01 00:19:40 +0530

    todo list added

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos"/>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos';
+
+export default {
+  name: 'app',
+  components: {
+    Todos
+  },
+  data(){
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Two",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Three",
+          completed: true
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body{
+  font-family: serif, Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
